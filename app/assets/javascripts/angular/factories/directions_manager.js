635f15d8609bedc58d6bbd8cd9e5449284d2466a
Maria Halacheva <mrhalacheva@gmail.com>
2015-08-02 20:18:12 +0300

    Load addresses by reverse geocoding when clicking on the map

--- app/assets/javascripts/angular/factories/directions_manager.js
+++ app/assets/javascripts/angular/factories/directions_manager.js
@@ -14,37 +14,62 @@ angular.module('directionServicesApp').factory('DirectionsManager', ['$window', 
 
       loadSearchServices: function(fromInput, toInput) {
         var that = this;
+        this.geocoder = new google.maps.Geocoder();
 
+        this.fromInput = fromInput;
         this.fromSearchBox = new google.maps.places.SearchBox(fromInput[0]);
         this.fromPlacesService = new google.maps.places.PlacesService(fromInput[0]);
         google.maps.event.addListener(this.fromSearchBox, 'places_changed', function() {
-          that.positionMarker(that.fromPlacesService, 'from', fromInput);
+          that.addressSearch(that.fromPlacesService, 'from', that.fromInput);
         });
 
+        this.toInput = toInput;
         this.toSearchBox = new google.maps.places.SearchBox(toInput[0]);
         this.toPlacesService = new google.maps.places.PlacesService(toInput[0]);
         google.maps.event.addListener(this.toSearchBox, 'places_changed', function() {
-          that.positionMarker(that.toPlacesService, 'to', toInput);
+          that.addressSearch(that.toPlacesService, 'to', that.toInput);
+        });
+
+        google.maps.event.addListener(this.map, 'click', function(event) {
+          that.locationSearch(event.latLng);
         });
       },
 
-      positionMarker: function(service, type, input) {
+      addressSearch: function(service, type, input) {
         var that = this;
         service.textSearch({ query: input.val() }, function(places, status) {
-          that.removeMarker(type);
-
-          if (places.length > 0) {
-            var location = places[0].geometry.location;
-            var title = places[0].formatted_address;
-
-            that.markers[type] = new google.maps.Marker({
-              position: location,
-              title: title.toString(),
-              map: that.map
-            });
-            that.updateViewPort();
-          }
+          that.onPlacesFound(type, places);
         });
+      },
+
+      locationSearch: function(location) {
+        var that = this;
+        var searchBox = this.selectSearchBox();
+
+        if (typeof searchBox !== 'undefined') {
+          this.geocoder.geocode({ location: location }, function(places, status) {
+            that.onPlacesFound(searchBox.type, places, searchBox.input);
+          });
+        }
+      },
+
+      positionMarker: function(type, place) {
+        var that = this;
+        var location = place.geometry.location;
+        var title = place.formatted_address;
+
+        this.markers[type] = new google.maps.Marker({
+          position: location,
+          title: title,
+          map: this.map,
+          draggable: true
+        });
+
+        google.maps.event.addListener(this.markers[type], 'dragend', function(event) {
+          that.locationSearch(event.latLng);
+        });
+
+        this.updateViewPort();
       },
 
       updateViewPort: function() {
@@ -64,6 +89,27 @@ angular.module('directionServicesApp').factory('DirectionsManager', ['$window', 
         if (typeof this.markers[type] !== 'undefined') {
           this.markers[type].setMap(null);
           this.markers[type] = undefined;
+        }
+      },
+
+      selectSearchBox: function() {
+        if (typeof this.markers.from === 'undefined') {
+          return { type: 'from',
+                   input: this.fromInput };
+        } else if (typeof this.markers.to === 'undefined') {
+          return { type: 'to',
+                   input: this.toInput };
+        }
+      },
+
+      onPlacesFound: function(type, places, input) {
+        this.removeMarker(type);
+
+        if (places.length > 0) {
+          this.positionMarker(type, places[0]);
+          if (typeof input !== 'undefined') {
+            input.val(places[0].formatted_address);
+          }
         }
       }
     };
